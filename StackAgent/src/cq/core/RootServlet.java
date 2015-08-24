8d02b19f925de2fca133e0c3cf65df4ea7788b52
chenqing24 <chenqing24@163.com>
2015-08-24 23:55:42 +0800

    debugger ioc in rootservlet

--- StackAgent/src/cq/core/RootServlet.java
+++ StackAgent/src/cq/core/RootServlet.java
@@ -1,7 +1,5 @@
 package cq.core;
 
-import static cq.core.ClassForName.createInstance;
-
 import java.io.IOException;
 
 import javax.servlet.ServletConfig;
@@ -13,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import cq.core.exception.ActionException;
-import cq.core.exception.ClassForNameException;
 import cq.core.ioc.BeanFactory;
 import cq.core.route.Route;
 
@@ -23,7 +20,6 @@ import cq.core.route.Route;
 @WebServlet("/RootServlet")
 public class RootServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String ROUTES = "routes";	// 配置中绑定的路由
     public static final String INIT_FACTORY_NAME="_MY_BEAN_FACTORY";
 	
 	protected transient ServletContext context;
@@ -33,31 +29,26 @@ public class RootServlet extends HttpServlet {
 	 * 从web.xml中读取配置
 	 */
     @Override
-    public void init(ServletConfig config) throws ServletException {
+	public void init(ServletConfig config) throws ServletException {
 		super.init();
 		BeanFactory beanFactory = null;
-        context = config.getServletContext();
-        final String routesParam = config.getInitParameter(ROUTES);
-        try {
-        	// ioc加载
-        	String configLocation =config.getInitParameter("configLocation");  
-            if (configLocation==null) {  
-                //如果在web.xml里面没有配置properties文件的名字则  
-                beanFactory=new BeanFactory();  
-            }else {  
-                beanFactory=new BeanFactory(configLocation);  
-            }  
-            config.getServletContext().setAttribute(INIT_FACTORY_NAME, beanFactory);
-            
-            
-        	route = createInstance(routesParam, Route.class);
-        	
-        } catch (ActionException e) {
-            throw new ServletException(e);
-        } catch (ClassForNameException e) {
-        	throw new ServletException(e);
+		context = config.getServletContext();
+		try {
+			// ioc加载
+			String configLocation = config.getInitParameter("configLocation");
+			if (configLocation == null) {
+				// 如果在web.xml里面没有配置properties文件的名字则
+				beanFactory = new BeanFactory();
+			} else {
+				beanFactory = new BeanFactory(configLocation);
+			}
+			config.getServletContext().setAttribute(INIT_FACTORY_NAME, beanFactory);
+
+			route = (Route) beanFactory.getBean("AppRoutes");
+		} catch (ActionException e) {
+			throw new ServletException(e);
 		}
-    }
+	}
     
     /**
      * 统一处理请求
