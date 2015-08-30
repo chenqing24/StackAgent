f0f422799e416d1b1e7644b53db1ab2a1aab3557
chenqing24 <chenqing24@163.com>
2015-08-31 00:03:41 +0800

    add DbTool into ioc

--- StackAgent/src/cq/action/TestAction.java
+++ StackAgent/src/cq/action/TestAction.java
@@ -1,6 +1,12 @@
 package cq.action;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import cq.core.RootServlet;
 import cq.core.action.BaseAction;
+import cq.core.ioc.BeanFactory;
+import cq.core.orm.DbTool;
 
 /**
  * 测试Action
@@ -16,6 +22,17 @@ public class TestAction extends BaseAction {
 	public void run() {
 		this.putTplMap("hi", "hello");
         try {
+        	BeanFactory beanFactory = (BeanFactory) this.context().getAttribute(RootServlet.INIT_FACTORY_NAME);
+        	DbTool dt = (DbTool) beanFactory.getBean("DbTool");
+        	Statement st =  dt.getConn().createStatement();
+	        ResultSet rs= st.executeQuery("SELECT * FROM TEST ORDER BY ID");
+	        while(rs.next()){
+	            System.out.println(rs.getString("NAME"));
+	        }
+	        
+	        rs.close();
+	        st.close();
+	        
 			this.tplPrint("hi");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
